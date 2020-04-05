9a54250dd48ca1ce7bec94603f8e416ee7170a68
Ioannis Boras <i_boras@hotmail.com>
2020-04-06 00:50:44 +0300

    make generic parser. Add first parse of everythong

--- main.py
+++ main.py
@@ -8,22 +8,8 @@ import zipfile
 import shutil
 from pykml import parser
 
-
-class Temaxio(object):
-    Name = ""
-    FEATUREID = ""
-    SBPI_ID_NO = ""
-    ΚΩΔΙΚΟΣ_ΕΠΑΡΧΙΑΣ = ""
-    ΚΩΔΙΚΟΣ_ΔΗΜΟΥ_ΚΟΙΝΟΤΗΤΑΣ = ""
-    ΚΩΔΙΚΟΣ_ΕΝΟΡΙΑΣ = ""
-    ΚΩΔΙΚΟΣ_ΤΜΗΜΑΤΟΣ = ""
-    ΦΥΛΛΟ = ""
-    ΣΧΕΔΙΟ = ""
-    ΑΡΙΘΜΟΣ_ΤΕΜΑΧΙΟΥ = ""
-    SRC_SL_CODE = ""
-    ΠΗΓΗ = ""
-    ΕΜΒΑΔΟ_ΤΕΜΑΧΙΟΥ = ""
-    coordinates= ""
+class Expando(object):
+    pass
 
     # The class "constructor" - It's actually an initializer 
     def __init__(self, name):
@@ -33,12 +19,13 @@ class Temaxio(object):
 def CSVImporter(items, file):
 
     with open('data/CSV/' + file, 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['Name','FEATUREID','SBPI_ID_NO','ΚΩΔΙΚΟΣ_ΕΠΑΡΧΙΑΣ','ΚΩΔΙΚΟΣ_ΔΗΜΟΥ_ΚΟΙΝΟΤΗΤΑΣ','ΚΩΔΙΚΟΣ_ΕΝΟΡΙΑΣ','ΚΩΔΙΚΟΣ_ΤΜΗΜΑΤΟΣ','ΦΥΛΛΟ','ΣΧΕΔΙΟ','ΑΡΙΘΜΟΣ_ΤΕΜΑΧΙΟΥ','SRC_SL_CODE','ΠΗΓΗ','ΕΜΒΑΔΟ_ΤΕΜΑΧΙΟΥ','coordinates']
+        fieldnames = list(items[0].keys())
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         writer.writeheader()
         for temaxio in items:
-            writer.writerow({'Name': temaxio.Name, 'FEATUREID': temaxio.FEATUREID, 'SBPI_ID_NO': temaxio.SBPI_ID_NO, 'ΚΩΔΙΚΟΣ_ΕΠΑΡΧΙΑΣ': temaxio.ΚΩΔΙΚΟΣ_ΕΠΑΡΧΙΑΣ, 'ΚΩΔΙΚΟΣ_ΔΗΜΟΥ_ΚΟΙΝΟΤΗΤΑΣ': temaxio.ΚΩΔΙΚΟΣ_ΔΗΜΟΥ_ΚΟΙΝΟΤΗΤΑΣ, 'ΚΩΔΙΚΟΣ_ΕΝΟΡΙΑΣ': temaxio.ΚΩΔΙΚΟΣ_ΕΝΟΡΙΑΣ, 'ΚΩΔΙΚΟΣ_ΤΜΗΜΑΤΟΣ': temaxio.ΚΩΔΙΚΟΣ_ΤΜΗΜΑΤΟΣ, 'ΦΥΛΛΟ': temaxio.ΦΥΛΛΟ, 'ΣΧΕΔΙΟ': temaxio.ΣΧΕΔΙΟ, 'ΑΡΙΘΜΟΣ_ΤΕΜΑΧΙΟΥ': temaxio.ΑΡΙΘΜΟΣ_ΤΕΜΑΧΙΟΥ, 'SRC_SL_CODE': temaxio.SRC_SL_CODE, 'ΠΗΓΗ': temaxio.ΠΗΓΗ, 'ΕΜΒΑΔΟ_ΤΕΜΑΧΙΟΥ': temaxio.ΕΜΒΑΔΟ_ΤΕΜΑΧΙΟΥ, 'coordinates': temaxio.coordinates})
+            writer.writerow(temaxio)
+
 
 def copyKMZandrenametoZIP(filename):
     sourceFile = "data/KMZ/" + filename
@@ -68,14 +55,21 @@ def deleteCreated():
     shutil.rmtree('data/UNZIP')
 
 def Parser(docxml):
-    temaxia = []
+    items = []
     #tree = ET.parse(docxml)
     with open(docxml, encoding='utf-8') as f:
         doc = parser.parse(f)
         root = doc.getroot()   
         for placemark in root.Document.Folder.Placemark:
-            temaxio = Temaxio(placemark.name)
-            temaxio.coordinates = (placemark.MultiGeometry.Polygon.outerBoundaryIs.LinearRing.coordinates)
+            item = {}
+            item["Name"] = placemark.name
+            try:
+                item["coordinates"] = placemark.MultiGeometry.Polygon.outerBoundaryIs.LinearRing.coordinates
+            except:
+                try:
+                    item["coordinates"] = placemark.Point.coordinates
+                except:
+                    item["coordinates"] = placemark.MultiGeometry.LineString.coordinates
 
             desc = placemark.description.text
             table = etree.HTML(desc).find("body/table/tr/td/table")
@@ -83,59 +77,25 @@ def Parser(docxml):
             for row in rows:
                 values = [col.text for col in row]
                 if (len(values) == 2):
-                    if (values[0] == 'FEATUREID'):
-                        temaxio.FEATUREID = values[1]
-                        
-                    elif (values[0] == 'SBPI_ID_NO'):
-                        temaxio.SBPI_ID_NO = values[1]
-                        
-                    elif (values[0] == 'ΚΩΔΙΚΟΣ ΕΠΑΡΧΙΑΣ'):
-                        temaxio.ΚΩΔΙΚΟΣ_ΕΠΑΡΧΙΑΣ = values[1]
-                        
-                    elif (values[0] == 'ΚΩΔΙΚΟΣ ΔΗΜΟΥ_ΚΟΙΝΟΤΗΤΑΣ'):
-                        temaxio.ΚΩΔΙΚΟΣ_ΔΗΜΟΥ_ΚΟΙΝΟΤΗΤΑΣ = values[1]
-                        
-                    elif (values[0] == 'ΚΩΔΙΚΟΣ ΕΝΟΡΙΑΣ'):
-                        temaxio.ΚΩΔΙΚΟΣ_ΕΝΟΡΙΑΣ = values[1]
-                        
-                    elif (values[0] == 'ΚΩΔΙΚΟΣ ΤΜΗΜΑΤΟΣ'):
-                        temaxio.ΚΩΔΙΚΟΣ_ΤΜΗΜΑΤΟΣ = values[1]
-                        
-                    elif (values[0] == 'ΦΥΛΛΟ'):
-                        temaxio.ΦΥΛΛΟ = values[1]
-                        
-                    elif (values[0] == 'ΣΧΕΔΙΟ'):
-                        temaxio.ΣΧΕΔΙΟ = values[1]
-                        
-                    elif (values[0] == 'ΑΡΙΘΜΟΣ ΤΕΜΑΧΙΟΥ'):
-                        temaxio.ΑΡΙΘΜΟΣ_ΤΕΜΑΧΙΟΥ = values[1]
-                        
-                    elif (values[0] == 'SRC_SL_CODE'):
-                        temaxio.SRC_SL_CODE = values[1]
-                        
-                    elif (values[0] == 'ΠΗΓΗ'):
-                        temaxio.ΠΗΓΗ = values[1]
-                        
-                    elif (values[0] == 'ΕΜΒΑΔΟ ΤΕΜΑΧΙΟΥ'):
-                        temaxio.ΕΜΒΑΔΟ_ΤΕΜΑΧΙΟΥ = values[1]
-            temaxia.append(temaxio)
-        return temaxia
+                    item[values[0]] = values[1]
+     
+            items.append(item)
+        return items
 
 def main():
     """
     Open the KML. Read the KML. Open a CSV file. Process a coordinate string to be a CSV row.
     """ 
-    #file = 'ΤΕΜΑΧΙΑ_0'
-  #  file = 'ΚΤΗΡΙΑΓΚΒΔ_17'
     try:
-        for filename in os.listdir('data/KMZ'):
-            if filename.endswith(".kmz"): 
+         for filename in os.listdir('data/KMZ'):
+             if filename.endswith(".kmz"): 
                 zipefile = copyKMZandrenametoZIP(filename)
-              #  docxml = unzipFile(zipefile, filename)
-      #  temaxia = Parser(docxml)
-      #  CSVImporter(temaxia,file + '.csv')
+                docxml = unzipFile(zipefile, filename)
+                temaxia = Parser(docxml)
+                base = os.path.splitext(filename)[0]
+                CSVImporter(temaxia,base + '.csv')
     finally:
-       # deleteCreated()
+       deleteCreated()
        print("g")
     
 
